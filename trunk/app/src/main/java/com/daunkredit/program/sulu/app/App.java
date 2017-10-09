4b0bb6b7e7b473ae7dba2f3aaac7830b51c1d1f3
huilv <hui.lv@avazu.net>
2017-10-09 20:41:21 +0800

    mmmmmmmmmmmmmmmmmmmmmmmmmm

--- trunk/app/src/main/java/com/daunkredit/program/sulu/app/App.java
+++ trunk/app/src/main/java/com/daunkredit/program/sulu/app/App.java
@@ -76,4 +76,4 @@ public class App extends MultiDexApplication {
     }
 
 }
-/////////////////////////////////////////
+/////////////////////////////////////////mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
